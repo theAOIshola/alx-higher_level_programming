5d680c6b7f4e5b08c96db232a204d8e399bdc35d
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-03-28 12:29:23 +0000

    use python exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+	index = 0
+	for x in range(x):
+		try:
+			print("{}".format(my_list[x]), end="")
+			index += 1
+		except IndexError:
+			break
+	print("")
+	return (index)
---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+
+def safe_print_integer(value):
+	try:
+		print("{:d}".format(value))
+		return (True)
+	except (TypeError, ValueError):
+		return (False)
