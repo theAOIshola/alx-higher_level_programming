3fa6d9e124d990a1eb9d7e15ec3ae861c66eb420
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-04-07 08:14:09 +0000

    hot fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
     
     def perimeter(self):
         """Calculates the perimeter of rectangle"""
-        if self.__width = 0 or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             perimeter = 0
         else:
             perimeter = 2 * (self.__width + self.__height)
